d6bf7c87cd0b5bce20c0ee74db7e3c38602c7678
zhao <1459666160@qq.com>
2019-04-24 20:10:16 +0800

    第一次发布的内容

---
+++ js/lun.js
@@ -0,0 +1,14 @@
+$(function(){
+    var myElement=document.getElementById('carousel-example-generic');
+    var hm=new Hammer(myElement);
+    hm.on("swipeleft",function(){
+        $('#carousel-example-generic').carousel('next');
+    });
+    hm.on("swiperight",function(){
+        $('#carousel-example-generic').carousel('prev');
+    })
+});
+
+
+
+
